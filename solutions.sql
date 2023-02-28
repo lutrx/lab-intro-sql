4734869fa71bcbbd75e3c1bb484ea0bd70369dfd
lutrx <lisa.bieker@web.de>
2023-02-28 16:50:05 +0100

    created script with querys for the tasks

---
+++ solutions.sql
@@ -0,0 +1,18 @@
+USE sakila;
+
+SELECT * FROM actor;
+
+SELECT * FROM film;
+
+SELECT * FROM customer;
+
+SELECT title FROM film ;
+
+SELECT name FROM language;
+
+SELECT COUNT(*) FROM store;
+
+SELECT COUNT(*) FROM staff;
+
+SELECT first_name FROM staff;
+
